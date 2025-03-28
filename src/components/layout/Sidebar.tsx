ef84d7b395ccc9f01e466ac526eaf9fba21eb01d
zandermmcg <zandermmcg@gmail.com>
2025-03-27 22:01:56 -0500

    added directory placeholders

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+// placeholder file
+
+export default function SideBar() {
+    return (
+        <div>
+            |
+        </div>
+    );
+}
---
+++ src/components/ui/Movie.module.tsx
@@ -0,0 +1,12 @@
+// placeholder file
+import styles from "@styles/Movie.module.css";
+
+export default function Movie() {
+    return (
+        <div className={styles.container}>
+            100Gar
+            Rating: 10/5
+            Actors: Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar, Gar
+        </div>
+    )
+}
