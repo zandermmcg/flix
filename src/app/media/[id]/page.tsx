4f11a7fa38ae6293efe1ac991802a43c8e6275af
zandermmcg <zandermmcg@gmail.com>
2025-04-14 17:13:16 -0500

    fixed issues

--- src/app/media/[id]/page.tsx
+++ src/app/media/[id]/page.tsx
@@ -1,66 +1,16 @@
-"use client";
-import Link from "next/link";
-import { fetchMediaPageData, postFavorite, postShare } from '@/lib/api';
-import Image from "next/image";
-import { FaHeart, FaShare } from 'react-icons/fa';
-import styles from '@styles/components/MediaCard.module.css';
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import MediaPage from "@components/pages/MediaPage.module";
 
 interface MediaPageProps {
-    id: number
+    params: {
+        id: number
+    }
 }
 
-// Recieved from fetchMediaCardData
-interface MediaPageData {
-    id: number,
-    title: string,
-    imageURL: string,
-    rating: number,
-    bio: string,
-    trailer_link: string,
-    where_to_watch: string // should probably create separate type for where to watch to store different streaming services and stuff
-}
-
-// TODO: Implement
-
-export default async function MediaPage({ params }: { params: MediaPageProps }) {
-    // const [media, setMedia] = useState<MediaPageData | null>(null);
-
-    // // Re-fetches data and re-render's component if the "id" prop changes (and on initial render)
-    // useEffect(() => {
-    //     async function getMedia() {
-    //         const data = await fetchMediaPageData(params.id);
-    //         setMedia(data);
-    //     }
-    //     getMedia();
-    // }, [params]);
-
-    const media = await fetchMediaPageData(params.id);
-
-    // Loading state, can be prettied up later
-    if (!media) return (
-        <div className={styles.container}>
-            Loading...
-        </div>
-    );
-
-
+export default function MediaPageRouter({ params }: MediaPageProps ) {
+    const id = Number(params.id);
     return (
-        <div className={styles.mediapage}>
-            {media.title} <br></br>
-            <div className={styles.overlayButtons}>
-                <button className={styles.quickFavoriteButton}
-                        // could reuse the functionality of hovering to make the buttons change color slightly like dim or something
-                        // onMouseEnter={() => setIsHoveredButton(true)}
-                        // onMouseLeave={() => setIsHoveredButton(false)}
-                        onClick={() => postFavorite(params.id)}
-                ><FaHeart/></button>
-                <button className={styles.quickShareButton}
-                        // onMouseEnter={() => setIsHoveredButton(true)}
-                        // onMouseLeave={() => setIsHoveredButton(false)}
-                        onClick={() => postShare(params.id)}
-                ><FaShare/></button>
-            </div>
+        <div>
+            <MediaPage id={id}></MediaPage>
         </div>
-    );
+    )
 }
