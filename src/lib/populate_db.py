11c313abec0768d22ea7605f7064dee5b3b6f96e
zandermmcg <zandermmcg@gmail.com>
2025-04-21 18:11:49 -0500

    updated populate_db

--- src/lib/populate_db.py
+++ src/lib/populate_db.py
@@ -12,8 +12,19 @@ API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BASE = "https://api.themoviedb.org/3"
 DYNAMO_TABLE = "flix"
 
-# dynamodb = boto3.resource("dynamodb")
-# table = dynamodb.Table(DYNAMO_TABLE)
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name='us-east-1'
+)
+
+dynamodb = session.resource("dynamodb")
+table = dynamodb.Table(DYNAMO_TABLE)
+
+def put_batch(items):
+    with table.batch_writer() as batch:
+        for item in items:
+            batch.put_item(Item=item)
 
 def fetch_by_id(id, type):
     url = f"{TMDB_BASE}/{type}/{id}"
@@ -26,6 +37,8 @@ def fetch_by_id(id, type):
 
 def transform(media, media_type):
     return {
+        "pk": "",
+        "sk": "",
         "mediaId": str(media["id"]),
         "type": media_type,
         "title": media.get("title") or media.get("name"),
@@ -65,62 +78,15 @@ def search_by_title(title):
 def searchall_movies(start, stop=1468452):
     items = []
     for id in tqdm(range(start, stop)):
-        print(id)
         try:
             movie = fetch_by_id(id, "movie")
             items.append(transform(movie, "movie"))
+
+            # Write batch to database if batch size >= 25
+            if len(items) >= 25:
+                put_batch(items)
+                items = []
+
             sleep(0.025)
         except:
             continue
-
-# print(search_by_title("The Woman in the Yard"))
-
-# movies = fetch_discover("movie", 3)['results']
-# movies = fetch_discover("movie", 1)
-# print(max([movie["id"] for movie in movies]))
-
-# def put_batch(items):
-#     with table.batch_writer() as batch:
-#         for item in items:
-#             batch.put_item(Item=item)
-
-# def fetch_and_store_all(media_type, max_pages=500):
-#     for page in tqdm(range(1, max_pages + 1)):
-#         data = fetch_discover(media_type, page)
-#         results = data.get("results", [])
-#         if not results:
-#             break
-#         items = [transform(m, media_type) for m in results]
-#         put_batch(items)
-#         sleep(0.25)
-
-# session = boto3.Session(
-#     aws_access_key_id='',
-#     aws_secret_access_key='',
-#     region_name='us-east-1'
-# )
-
-# dynamodb = session.resource('dynamodb')
-# table = dynamodb.Table('flix')
-
-# response = table.query(KeyConditionExpression=boto3.dynamodb.conditions.Key('pk').eq('USER#001') &
-#                                               boto3.dynamodb.conditions.Key('sk').eq('PROFILE'))
-
-# items = response["Items"]
-# print(items)
-
-
-
-# table = dynamodb.Table('flix')
-
-# item = {
-#     'pk': "USER#001",
-#     'sk': "PROFILE",
-#     'username': "contrambulance"
-# }
-
-# # Put the item into the table
-# response = table.put_item(Item=item)
-
-# # Print response
-# print("PutItem succeeded:", response)
